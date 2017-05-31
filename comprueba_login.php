e15c34a4a93cc5d28fc3d1edfb1b1cb2e0350bb6
alan_qp <alanquispe039@gmail.com>
2017-05-31 17:48:04 -0300

    LoginListo

--- comprueba_login.php
+++ comprueba_login.php
@@ -15,16 +15,40 @@ y dependiendo de eso manda a una pagina.
   <body>
     <?php
     try{
-      //instanciamos la clase PDO
+      //instanciamos la clase PDO, creamos la CONEXION
       $base=new PDO("mysql:host=localhost; dbname=colegio", "root","");
+
+      //creamos objetos de tipo exception esto le sirve a la bd para q informe al catch
+      //de cualquier error
       $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+      //le especificamos el juego de caracteres con acento
+      $base->exec("SET CHARACTER SET utf8");
+      //Consulta preparada usando marcadores, :login es un marcador
       $sql="SELECT * FROM USUARIOS_PASS WHERE USUARIOS= :login AND PASSWORD= :password";
+
+      //obtenemos el objeto "PDOStatement" en la variable $resultado
       $resultado=$base->prepare($sql);
-      //para evitar inyecciones:
+      //este objeto "PDOStatement" primero hay q ejecutarlo y luego
+      //hay q recorrer este resultado, ya q $resultado es una especie de tabla con los resultados
+      //de la consulta
+
+      //obtenemos los datos ingresados en el login evitando inyecciones
       $usu=htmlentities(addslashes($_POST["usu"]));
       $password=htmlentities(addslashes($_POST["password"]));
 
-      //si coincide
+      /*execute y fetch
+      Sin marcadores $resultado->execute(array($usu,$password));
+      Con marcadores $resultado->execute(array(":login"=>$usu, ":password"=>$password));
+      while($registro=$resultado->fetch(PDF::FETCH_ASSOC)){
+        echo $registro['campo1'];
+        echo $registro['campo1'];...
+      }
+      //liberando memoria
+      $resultado->closeCursor();
+      */
+
+      //Vinculamos los datos del login a la consulta SQL
       $resultado->bindValue(":login",$usu);
       $resultado->bindValue(":password",$password);
       $resultado->execute();
@@ -34,9 +58,11 @@ y dependiendo de eso manda a una pagina.
         session_start();
         $_SESSION["usuario"]=$_POST["usu"];
 
-        //validamos si es profesor o padre
-
-        //header("location: sector_padre.php");
+        //obtenemos el tipo de usuario: profesor o padre
+        $registro=$resultado->fetch(PDO::FETCH_ASSOC);
+        IF($registro['tipo']=="profesor"){
+          header("location: sector_profesor.php");
+        }else header("location: sector_padre.php");
       }else{
         //le redirigimos a la propia pagina web
         header("location:index.php");
